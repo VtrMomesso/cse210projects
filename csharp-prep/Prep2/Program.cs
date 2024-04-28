7dccc82c7f2b3ae1f8b0e96d51fba2b7fa04e67b
VtrMomesso <vtrmomesso@gmail.com>
2024-04-28 02:06:45 -0300

    Update Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Which is your grade percentage? ");
+        string gradep = Console.ReadLine();
+
+        int grade = int.Parse(gradep);
+
+        if (grade >= 90) 
+        {
+            Console.WriteLine("Your grade is A");
+        }
+        else if (grade >= 80) 
+        {
+            Console.WriteLine("Your grade is B");
+        }
+        else if (grade >= 70) 
+        {
+            Console.WriteLine("Your grade is C");
+        }
+        else if (grade >= 60) 
+        {
+            Console.WriteLine("Your grade is D");
+        }
+        else if (grade < 60)
+        {
+            Console.WriteLine("Your grade is F");
+        }
+        else
+        {
+            Console.WriteLine("I don't understand, please try it again. ");
+        }
     }
 }
