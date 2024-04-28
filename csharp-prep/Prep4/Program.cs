4b9cd3027a442559405b6b971024e1bbeb770a2c
VtrMomesso <vtrmomesso@gmail.com>
2024-04-28 04:24:22 -0300

    Update Program.cs

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+         List<int> numbersList = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int newNumber; 
+
+        while ((newNumber = Convert.ToInt32(Console.ReadLine())) != 0)
+        {
+            numbersList.Add(newNumber);
+            Console.WriteLine("Number added successfully!");
+                    
+        }                             
+                
+        int sum = numbersList.Sum();
+        Console.WriteLine($"Sum of numbers in the list: {sum}");
+    
+        double average = numbersList.Average();
+        Console.WriteLine($"Average of numbers in the list: {average}");
+                            
+        int max = numbersList.Max();
+        Console.WriteLine($"The largest number in the list: {max}");
+           
     }
 }
+
