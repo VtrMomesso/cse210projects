eb2b19a0527672b0b157bd506e2c8649a84ca9ba
VtrMomesso <vtrmomesso@gmail.com>
2024-04-28 02:25:35 -0300

    Update Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -11,19 +11,64 @@ class Program
 
         if (grade >= 90) 
         {
-            Console.WriteLine("Your grade is A");
+            if (grade >= 97)
+            {
+                Console.WriteLine("Your grade is A+");
+            }
+            else if (grade <= 93)
+            {
+                Console.WriteLine("Your grade is A-");
+            }
+            else
+            {
+                Console.WriteLine("Your grade is A");
+            }
+            
         }
         else if (grade >= 80) 
         {
-            Console.WriteLine("Your grade is B");
+            if (grade >= 87)
+            {
+                Console.WriteLine("Your grade is B+");
+            }
+            else if (grade <= 83)
+            {
+                Console.WriteLine("Your grade is B-");
+            }
+            else
+            {
+                Console.WriteLine("Your grade is B");
+            }
         }
         else if (grade >= 70) 
         {
-            Console.WriteLine("Your grade is C");
+            if (grade >= 77)
+            {
+                Console.WriteLine("Your grade is C+");
+            }
+            else if (grade <= 73)
+            {
+                Console.WriteLine("Your grade is C-");
+            }
+            else
+            {
+                Console.WriteLine("Your grade is C");
+            }
         }
         else if (grade >= 60) 
         {
-            Console.WriteLine("Your grade is D");
+            if (grade >= 67)
+            {
+                Console.WriteLine("Your grade is D+");
+            }
+            else if (grade <= 63)
+            {
+                Console.WriteLine("Your grade is D-");
+            }
+            else
+            {
+                Console.WriteLine("Your grade is D");
+            }
         }
         else if (grade < 60)
         {
@@ -32,6 +77,16 @@ class Program
         else
         {
             Console.WriteLine("I don't understand, please try it again. ");
+        }
+
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You Pass.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you don't pass, but you can try in the next semester.");
         }
     }
 }
