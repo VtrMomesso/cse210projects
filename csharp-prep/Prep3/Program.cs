58ef2a5a364b0110d2dab568e06d4bcd9dec5b7c
VtrMomesso <vtrmomesso@gmail.com>
2024-04-28 03:19:58 -0300

    Update Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        while (true)
+        {
+            Console.WriteLine("What is the magic number? ");
+            int magic_number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is your guess? ");
+            int guess = Convert.ToInt32(Console.ReadLine());
+
+            if (guess < magic_number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magic_number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You guess it!");
+                break;
+            }
+        }
     }
 }
