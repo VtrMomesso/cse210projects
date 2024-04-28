9ae612b148c16fdd3c42213548098a2a77c2c54b
VtrMomesso <vtrmomesso@gmail.com>
2024-04-28 03:32:06 -0300

    Update Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,13 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
+
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1, 101);
+        int guessCount = 0;
+
         while (true)
         {
-            Console.WriteLine("What is the magic number? ");
-            int magic_number = Convert.ToInt32(Console.ReadLine());
+ //           Console.WriteLine("What is the magic number? ");
+ //           int magic_number = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("What is your guess? ");
             int guess = Convert.ToInt32(Console.ReadLine());
+            guessCount++;
 
             if (guess < magic_number)
             {
@@ -23,6 +29,8 @@ class Program
             else
             {
                 Console.WriteLine("Congratulations! You guess it!");
+                Console.WriteLine($"It was in {guessCount} guesses.");
+                Console.WriteLine();
                 break;
             }
         }
