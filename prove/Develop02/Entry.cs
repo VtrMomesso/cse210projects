3e7f1b0f5198cbded4fa2a16fbc3f2199c2bff8e
VtrMomesso <vtrmomesso@gmail.com>
2024-05-10 03:51:04 -0300

    Update Entry.cs

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -4,6 +4,13 @@ public class Entry
     public string _PromptText;
     public string _EntryText;
 
+    public Entry(string date, string promptText, string entryText)
+    {
+        _Date = date;
+        _PromptText = promptText;
+        _EntryText = entryText;
+    }
+
     public void Display()
     {
         
