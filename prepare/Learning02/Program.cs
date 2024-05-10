128768b81bc2267c46f92b98319c6401be5b7360
VtrMomesso <vtrmomesso@gmail.com>
2024-05-09 22:48:46 -0300

    Update Program.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        public class Resume
+        {
+            public string _name;
+            
+        }
+
+        public class Job
+        {
+            public string _company;
+            public string _jobTitle;
+            public int _startYear;
+            public int _endYear;
+        }
     }
 }
