516f9a2314746fb19d498aa610ee29cae0636737
VtrMomesso <vtrmomesso@gmail.com>
2024-05-10 14:13:29 -0300

    Update Journal.cs

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,24 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
 public class Journal
 {
-    public List<Entry> _Entries;
-    
+    private List<Entry> _entries;
+
+    public Journal()
+    {
+        _entries = new List<Entry>();
+    }
+
     public void AddEntry(Entry newEntry)
     {
-
+        _entries.Add(newEntry);
     }
 
     public void DisplayAll()
     {
-
+        foreach (var entry in _entries)
+        {
+            entry.Display();
+        }
     }
 
     public void SaveToFile(string fileName)
     {
-
+        using (StreamWriter writer = new StreamWriter(fileName))
+        {
+            foreach (var entry in _entries)
+            {
+                writer.WriteLine($"{entry.Date},{entry.PromptText},{entry.EntryText}");
+            }
+        }
+        Console.WriteLine("Entries saved to file.");
     }
 
     public void LoadFromFile(string fileName)
     {
-
+        _entries.Clear();
+        string[] lines = File.ReadAllLines(fileName);
+        foreach (var line in lines)
+        {
+            string[] parts = line.Split(',');
+            _entries.Add(new Entry(parts[0], parts[1], parts[2]));
+        }
+        Console.WriteLine("Entries loaded from file.");
     }
 }
