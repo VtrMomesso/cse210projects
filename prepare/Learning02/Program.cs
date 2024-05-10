46a5b4fd269895cd00cd471ee5041795862ff629
VtrMomesso <vtrmomesso@gmail.com>
2024-05-09 22:58:50 -0300

    Update Program.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,24 +1,82 @@
 using System;
 using System.Security.Cryptography.X509Certificates;
 
+// Job class
+public class Job
+{
+    // Member variables
+    private string _company;
+    private string _jobTitle;
+    private int _startYear;
+    private int _endYear;
+
+    // Constructor
+    public Job(string company, string jobTitle, int startYear, int endYear)
+    {
+        _company = company;
+        _jobTitle = jobTitle;
+        _startYear = startYear;
+        _endYear = endYear;
+    }
+
+    // Display method
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
+
+// Resume class
+public class Resume
+{
+    // Member variables
+    private string _personName;
+    private List<Job> _jobs;
+
+    // Constructor
+    public Resume(string personName)
+    {
+        _personName = personName;
+        _jobs = new List<Job>();
+    }
+
+    // Method to add a job to the resume
+    public void AddJob(Job job)
+    {
+        _jobs.Add(job);
+    }
+
+    // Display method
+    public void Display()
+    {
+        Console.WriteLine($"Resume of {_personName}:");
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Create job instances
+        Job job1 = new Job("Microsoft", "Software Engineer", 2019, 2022);
+        Job job2 = new Job("Apple", "Manager", 2022, 2023);
 
-        public class Resume
-        {
-            public string _name;
-            
-        }
+        // Display job details
+        job1.Display();
+        job2.Display();
 
-        public class Job
-        {
-            public string _company;
-            public string _jobTitle;
-            public int _startYear;
-            public int _endYear;
-        }
+        // Create a resume instance
+        Resume myResume = new Resume("John Doe");
+
+        // Add jobs to the resume
+        myResume.AddJob(job1);
+        myResume.AddJob(job2);
+
+        // Display resume details
+        myResume.Display();
     }
 }
