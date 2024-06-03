fa68e98b62c96dc567d25dd6cd4f5920ceb730fe
VtrMomesso <vtrmomesso@gmail.com>
2024-06-03 00:45:23 -0300

    Update Program.cs

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,9 +1,58 @@
 using System;
 
-class Program
+namespace EternalQuest
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Develop05 World!");
+        static void Main(string[] args)
+        {
+            GoalManager goalManager = new GoalManager();
+            goalManager.Start();
+
+            bool running = true;
+            while (running)
+            {
+                Console.WriteLine("1. Display Player Info");
+                Console.WriteLine("2. List Goal Names");
+                Console.WriteLine("3. List Goal Details");
+                Console.WriteLine("4. Create Goal");
+                Console.WriteLine("5. Record Event");
+                Console.WriteLine("6. Save Goals");
+                Console.WriteLine("7. Load Goals");
+                Console.WriteLine("8. Exit");
+
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        goalManager.DisplayPlayerInfo();
+                        break;
+                    case "2":
+                        goalManager.ListGoalNames();
+                        break;
+                    case "3":
+                        goalManager.ListGoalDetails();
+                        break;
+                    case "4":
+                        goalManager.CreateGoal();
+                        break;
+                    case "5":
+                        goalManager.RecordEvent();
+                        break;
+                    case "6":
+                        goalManager.SaveGoals();
+                        break;
+                    case "7":
+                        goalManager.LoadGoals();
+                        break;
+                    case "8":
+                        running = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        break;
+                }
+            }
+        }
     }
 }
